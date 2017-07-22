36240ab4b3a145c244ae390517086613aeb322b9
adu windows <duuyidong@gmail.com>
2017-07-22 11:13:42 +0800

    fisrt commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ADU on 2017/4/25.
+ */
+public class Hello {
+    public void say(String s){
+        System.out.print("你好！");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ADU on 2017/4/25.
+ */
+public class Main {
+    public static void main (String args[]){
+        System.out.println("这是一个简单的程序");
+        Hello hello = new Hello();
+        hello.say("Hi");
+
+    }
+}
